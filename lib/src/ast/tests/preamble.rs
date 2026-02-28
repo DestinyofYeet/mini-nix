a43aae883a4a9a173e4abf06a0a30aca5765f2db
DestinyofYeet <ole@ole.blue>
2026-02-28 01:43:11 +0100

    chore(ast/tests): added more tests

--- lib/src/ast/tests/preamble.rs
+++ lib/src/ast/tests/preamble.rs
@@ -3,7 +3,9 @@ mod test {
     use crate::{
         ast::{
             parser::AstParser,
-            types::{Binary, Literal, Preamble, PreambleLetIn, PreambleType},
+            types::{
+                Binary, Expression, Literal, Preamble, PreambleLetIn, PreambleType, PreambleWith,
+            },
         },
         lexer::parse_text,
     };
@@ -27,6 +29,49 @@ mod test {
                     )]
                 }),
                 Literal::create(tokens[6].clone())
+            )
+        )
+    }
+
+    #[test]
+    pub fn let_in_assignment() {
+        let tokens = parse_text("let a = 2; in b = a;".to_string()).unwrap();
+        let mut parser = AstParser::new(tokens.clone());
+        let ast = parser.parse().unwrap();
+
+        assert_eq!(
+            ast,
+            Preamble::create(
+                PreambleType::LetIn(PreambleLetIn {
+                    expr: vec![Binary::create(
+                        Literal::create(tokens[1].clone()),
+                        tokens[2].clone(),
+                        Literal::create(tokens[3].clone())
+                    )]
+                }),
+                Binary::create(
+                    Literal::create(tokens[6].clone()),
+                    tokens[7].clone(),
+                    Literal::create(tokens[8].clone())
+                )
+            )
+        )
+    }
+
+    #[test]
+    pub fn with_primary() {
+        let tokens = parse_text("with a; b".to_string()).unwrap();
+
+        let mut parser = AstParser::new(tokens.clone());
+        let ast = parser.parse().unwrap();
+
+        assert_eq!(
+            ast,
+            Preamble::create(
+                PreambleType::With(PreambleWith {
+                    expr: Box::new(Literal::create(tokens[1].clone()))
+                }),
+                Literal::create(tokens[3].clone())
             )
         )
     }
